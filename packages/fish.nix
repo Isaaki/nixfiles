1925f68cdbde300b45bf6051dd5dd7942eba45b5
Isaaki <xisaacnewton1x@gmail.com>
2023-02-15 01:12:46 +0100

    add fish default shell

--- packages/fish.nix
+++ packages/fish.nix
@@ -7,10 +7,31 @@
   };
 
   programs.fish = {
-    enable = false;
+    enable = true;
+
+    plugins = [{
+      name="foreign-env";
+      src = pkgs.fetchFromGitHub {
+         owner = "oh-my-fish";
+         repo = "plugin-foreign-env";
+         rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+         sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
+      };
+    }];
+
+     shellInit = ''
+         # nix
+         fenv source ~/.nix-profile/etc/profile.d/nix.sh
+
+         if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+             fenv source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+         end
+     '';
 
     shellAliases = {
+      v = "$EDITOR";
       vi = "$EDITOR";
+      vim = "$EDITOR";
       gst = "git status";
       ".." = "cd ..";
       "..." = "cd ../../";
