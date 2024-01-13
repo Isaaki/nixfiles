747dd29a7cffba0d1542d4648414057329284876
Isak Mella <xisaacnewton1x@gmail.com>
2024-01-13 17:56:40 +0100

    Update configuration.nix

--- system/configuration.nix
+++ system/configuration.nix
@@ -27,7 +27,7 @@ in
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "isaaki"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
